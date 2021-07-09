92513333592ed8a8fa2679136e19c00b24ce9b64
Alexsandr <alunev.work@gmail.com>
2021-07-09 20:39:04 +0300

    New class Page 5

--- src/test/java/Page.java
+++ src/test/java/Page.java
@@ -57,6 +57,19 @@ public class Page {
                     //Вариант 2
                     $("h1").$("div").shouldNot(exist);
                 }
+                public class Page4 {
+                    @Test
+                    void SearchSelectors(){
+                        open("https://github.com/selenide/selenide");
+                        //Вариант 1
+                        $("div h1").should(exist);
+                    }
+                    @Test
+                    void SearchAnySelectors(){
+                        open("https://github.com/selenide/selenide");
+                        //Вариант 2
+                        $("h1").$("div").shouldNot(exist);
+                    }
 
 
 
